e93d2e9d676e3b4f226684ed7529f51ff9ccf11b
ArseniiTi <tiurin@sheridancollege.ca>
2019-01-24 17:05:36 -0500

    Field added studentid

--- src/netbiinds/software_desi.java
+++ src/netbiinds/software_desi.java
@@ -36,6 +36,15 @@ public String getColor() {
 this.color = color;
 this.letter = letter;
     }
+   private int studentID;
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
 
 }
 
